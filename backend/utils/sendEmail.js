9aaecc06b1b69551f42466bc8fa410d724001bda
jk_lama <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>
2024-03-11 23:28:02 +0545

    37 to 40 check next time not working currently

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,27 +1,36 @@
 const nodemailer = require('nodemailer')
+const dotenv = require('dotenv').config({ path: 'backend/config/config.env' })
 
 const sendEmail = async (options) => {
-  // 1) Create a transporter
-  var transport = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  })
+  try {
+    // 1) Create a transporter
+    var transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: false, // Change this to true if your email provider requires SSL/TLS
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    })
 
-  // 2) Define the email options
-  const message = {
-    from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_FROM}>`,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    html: `<div>${options.message}</div>`,
+    console.log('Transporter created')
+
+    // 2) Define the email options
+    const message = {
+      from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_FROM}>`,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+      html: `<div>${options.message}</div>`,
+    }
+
+    // 3) Actually send the email
+    await transporter.sendMail(message)
+    console.log('Email sent successfully')
+  } catch (error) {
+    console.error('Error sending email:', error)
   }
-  // 3) Actually send the email
-  await transport.sendMail(message)
-  console.log('email sent successfully')
 }
 
 module.exports = sendEmail
